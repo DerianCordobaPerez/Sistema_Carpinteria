68d0dd67bd315c6fe83ffbb8954a44f9c169c505
Ofilio Herdocia Aráuz <herdocia20@gmail.com>
2022-04-10 10:57:09 -0600

    Creando modelos basicos con sus migraciones.

---
+++ database/migrations/2022_04_10_152938_create_purchases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('purchase_type_id');
+            $table->foreign('purchase_type_id')
+                ->references('id')
+                ->on('purchase_types'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
